fc04f8515c9231a02fd676839373c081b0f96ad0
qiuqiangkong <qiuqiangkong@gmail.com>
2018-07-16 10:05:23 +0100

    update

--- runme.sh
+++ runme.sh
@@ -4,5 +4,8 @@ WORKSPACE="/vol/vssp/msos/qk/workspaces/energy_disaggregation"
 # Pack csv files to hdf5
 python pack_csv_to_hdf5.py --dataset_dir=$DATASET_DIR --workspace=$WORKSPACE
 
-# 
-python tmp01.py train --workspace=$WORKSPACE
+# Train
+CUDA_VISIBLE_DEVICES=1 python main_pytorch.py train --workspace=$WORKSPACE --cuda
+
+# Inference
+CUDA_VISIBLE_DEVICES=1 python main_pytorch.py inference --workspace=$WORKSPACE --iteration=1000 --cuda
