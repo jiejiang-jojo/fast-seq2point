188038bc68c59da4fd2ae6edad765af8b9d71b02
Jie Jiang <jiejiang.jojo@gmail.com>
2018-08-13 09:47:55 +0100

    Fix children module registration in parent, channel dimension and typos

--- models.py
+++ models.py
@@ -27,11 +27,15 @@ def move_data_to_gpu(x, cuda):
 
 def init_layer(layer):
     """Initialize a Linear or Convolutional layer. """
-    
+
     if layer.weight.ndimension() == 4:
         (n_out, n_in, height, width) = layer.weight.size()
         n = n_in * height * width
-        
+
+    elif layer.weight.ndimension() == 3:
+        (n_out, n_in, width) = layer.weight.size()
+        n = n_in * width
+
     elif layer.weight.ndimension() == 2:
         (n_out, n) = layer.weight.size()
 
@@ -196,8 +200,9 @@ class DilatedResidualBlock(nn.Module):
         super(DilatedResidualBlock, self).__init__()
         self.out_channels = out_channels
         self.skip_channels = skip_channels
-        self.dilated_conv = nn.Conv1d(out_channels, 2 * out_channels, kernel_size=kernel_size, dilation=dilation, padding=(dilation, dilation), bias=bias)
+        self.dilated_conv = nn.Conv1d(out_channels, 2 * out_channels, kernel_size=kernel_size, dilation=dilation, padding=dilation, bias=bias)
         self.mixing_conv = nn.Conv1d(out_channels, out_channels + skip_channels, kernel_size=1, bias=False)
+        self.init_weights()
 
     def init_weights(self):
         init_layer(self.dilated_conv)
@@ -205,14 +210,14 @@ class DilatedResidualBlock(nn.Module):
 
     def forward(self, data_in):
         out = self.dilated_conv(data_in)
-        out1 = out.narrow(-1, 0, self.out_channels)
-        out2 = out.narrow(-1, self.out_channels, 2 * self.out_channels)
+        out1 = out.narrow(-2, 0, self.out_channels)
+        out2 = out.narrow(-2, self.out_channels, self.out_channels)
         tanh_out = F.tanh(out1)
         sigm_out = F.sigmoid(out2)
         data = F.mul(tanh_out, sigm_out)
         data = self.mixing_conv(data)
-        res = data.narrow(-1, 0, self.out_channels)
-        skip = data.narrow(-1, 0, self.skip_channels).narrow(...)
+        res = data.narrow(-2, 0, self.out_channels)
+        skip = data.narrow(-2, self.out_channels, self.skip_channels)
         res = res + data_in
         return res, skip
 
@@ -230,20 +235,21 @@ class WaveNet2(nn.Module):
         skip_channels = 8
 
         self.causal_conv = nn.Conv1d(1, channels, kernel_size=1, bias=False)
-        self.blocks = [DilatedResidualBlock(channels, skip_channels, WaveNet2.kernel_size, 2*i, True)
+        self.blocks = [DilatedResidualBlock(channels, skip_channels, WaveNet2.kernel_size, 2**i, True)
                        for i in range(WaveNet2.layers)]
-        self.penultimate_conv = nn.Conv1d(skip_channels, 64, kernel_size=WaveNet2.kernel_size, padding=(kernel_size-1)//2, bias=True)
-        self.final_conv = nn.Conv1d(64, 1, kernel_size=WaveNet2.kernel_size, padding=(kernel_size-1)//2, bias=True)
+        for i, block in enumerate(self.blocks):
+            self.add_module(f"dilatedConv{i}", block)
+        self.penultimate_conv = nn.Conv1d(skip_channels, 64, kernel_size=WaveNet2.kernel_size, padding=(WaveNet2.kernel_size-1)//2, bias=True)
+        self.final_conv = nn.Conv1d(64, 1, kernel_size=WaveNet2.kernel_size, padding=(WaveNet2.kernel_size-1)//2, bias=True)
+        self.init_weights()
 
     def init_weights(self):
         init_layer(self.causal_conv)
         init_layer(self.penultimate_conv)
         init_layer(self.final_conv)
-        for block in self.blocks:
-            block.init_weights()
 
     def forward(self, data_in):
-        data_in = data_in.squeeze(-2)
+        data_in = data_in.view(data_in.shape[0], 1, data_in.shape[1])
         data_out = self.causal_conv(data_in)
         skip_connections = []
         for block in self.blocks:
@@ -254,5 +260,5 @@ class WaveNet2(nn.Module):
             skip_out = skip_out + skip_other
         data_out = F.relu(skip_out)
         data_out = self.penultimate_conv(data_out)
-        data_out = self.final_conv(data_out).unsqueeze(-1)
-        return data_out.narrow(1, WaveNet2.seq_len//2, data_out.size()[1]-WaveNet2.seq_len+1)
+        data_out = self.final_conv(data_out)
+        return data_out.narrow(-1, WaveNet2.seq_len//2, data_out.size()[-1]-WaveNet2.seq_len+1)
