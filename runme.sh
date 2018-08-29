8b081167e89d4b96399dfe915e4375dd1a8e2255
qiuqiangkong <qiuqiangkong@gmail.com>
2018-08-29 14:22:27 +0100

    runme.sh

--- runme.sh
+++ runme.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #DATASET_DIR="/vol/vssp/msos/qk/JieJiang/REFIT/edhc/csv"
-DATASET_DIR="$(pwd)/ukdale"
-WORKSPACE="$(pwd)"
+DATASET_DIR="/vol/vssp/msos/qk/JieJiang/REFIT/edhc/ukdale"
+WORKSPACE="/vol/vssp/msos/qk/JieJiang/REFIT/edhc"
 
 # Pack csv files to hdf5
 #python3 pack_csv_to_hdf5.py --dataset_dir=$DATASET_DIR --workspace=$WORKSPACE
