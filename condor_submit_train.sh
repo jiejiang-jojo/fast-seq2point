23b8ff91bc6ee385366c04603c92a5630815fb09
Jie Jiang <jiejiang.jojo@gmail.com>
2018-09-02 11:57:07 +0100

    Add the scripts for upload jobs to HTCondor

---
+++ condor_submit_train.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# To use this script a template condor job description is needed.
+# It should contain everything (including the executable path) but aruguments
+# which will be supplied by this script.
+
+if [ "$#" -ne 2 ]; then
+  echo 'condor_submit_train.sh <template> <config file> <conda env path> [--pm- ...]'
+  exit
+fi
+
+TEMPLATE="$1"
+shift
+
+CONFIG="$1"
+shift
+
+CONDA_ENV_PATH="$1"
+shift
+
+WORKSPACE="$(pwd)"
+
+ARGUMENTS="arguments = $WORKSPACE/$CONFIG $CONDA_ENV_PATH $WORKSPACE $*"
+
+echo "$ARGUMENTS"
+condor_submit -a "$ARGUMENTS" "$TEMPLATE"
