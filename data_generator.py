Fix data binarization with evaluation metrics and input normalization

data_generator.py
@@ -4,7 +4,7 @@ import time
 import logging
 #import matplotlib.pyplot as plt
 
-from utilities import calculate_scalar, scale, inverse_scale
+from utilities import calculate_scalar, scale, inverse_scale, binarize
 
 
 class DataGenerator(object):
@@ -118,8 +118,6 @@ class DataGenerator(object):
             aggregates = np.concatenate(aggregates, axis=0)
             targets = np.concatenate(targets, axis=0)
 
-            if self.binary_threshold is not None:
-                targets = ((targets - self.binary_threshold) > 0).astype('float')
             return aggregates, targets
 
     def generate(self):
@@ -154,9 +152,11 @@ class DataGenerator(object):
             batch_x = self.train_x[batch_x_indexes_2d]
             batch_y = self.train_y[batch_y_indexes_2d]
 
-            if self.binary_threshold is None:
-                # Transform
-                batch_x = self.transform(batch_x)
+            # Normalize input
+            batch_x = self.transform(batch_x)
+            if self.binary_threshold is not None:
+                batch_y = binarize(batch_y, self.binary_threshold)
+            else:
                 batch_y = self.transform(batch_y)
 
             yield batch_x, batch_y
@@ -205,9 +205,11 @@ class DataGenerator(object):
             batch_x = x[batch_x_indexes_2d]
             batch_y = y[batch_y_indexes_2d]
 
-            if self.binary_threshold is None:
-                # Transform
-                batch_x = self.transform(batch_x)
+            # Normalize input
+            batch_x = self.transform(batch_x)
+            if self.binary_threshold is not None:
+                batch_y = binarize(batch_y, self.binary_threshold)
+            else:
                 batch_y = self.transform(batch_y)
 
             yield batch_x, batch_y
@@ -278,6 +280,8 @@ class TestDataGenerator(DataGenerator):
         return np.concatenate((np.zeros(pad_num), x, np.zeros(pad_num)))
 
     def get_target(self):
+        if self.binary_threshold is not None:
+            return binarize(self.target, self.binary_threshold)
         return self.target
 
     def get_source(self):

main_pytorch.py
@@ -31,19 +31,14 @@ def loss_func_binary(output, target):
     return F.binary_cross_entropy(output, target)
 
 
-def binarize(tensor):
-    return np.floor(tensor + 0.5)
-
-
 def accuracy(Y, Y_hat):
     return (Y == Y_hat).sum() / Y.size
 
 
 def binary_metrics(outputs, targets):
-    binary_outputs = binarize(outputs)
-    (tp, fn, fp, tn) = tp_fn_fp_tn(binary_outputs, targets, 0.5)
-    precision_value = precision(binary_outputs, targets, 0.5)
-    recall_value = recall(binary_outputs, targets, 0.5)
+    (tp, fn, fp, tn) = tp_fn_fp_tn(outputs, targets)
+    precision_value = precision(outputs, targets)
+    recall_value = recall(outputs, targets)
     f1_score = f_value(precision_value, recall_value)
 
     auc = roc_auc(outputs, targets)

utilities.py
@@ -18,6 +18,10 @@ def get_filename(path):
     return na
 
 
+def binarize(tensor, threshold=0.5):
+    return ((tensor - threshold) > 0).astype('float')
+
+
 def allocate_experiment_id(log_dir, id_seq_filename='._exp_id_seq'):
     seq_fd_path = os.path.join(log_dir, id_seq_filename)
     if not os.path.exists(seq_fd_path):
@@ -98,34 +102,32 @@ def mean_absolute_error(output, target):
 def signal_aggregate_error(output, target):
 
     return np.abs(np.sum(output) - np.sum(target))/np.sum(target)
-    
-    
-def tp_fn_fp_tn(output, target, thres):
-    output_digt = np.zeros_like(output)
-    output_digt[np.where(output > thres)] = 1.
-    tp = np.sum(output_digt + target > 1.5)
+
+
+def tp_fn_fp_tn(output, target):
+    tp = np.sum(output + target > 1.5)
     fn = np.sum(target - output > 0.5)
-    fp = np.sum(output_digt - target > 0.5)
-    tn = np.sum(output_digt + target < 0.5)
+    fp = np.sum(output - target > 0.5)
+    tn = np.sum(output + target < 0.5)
     return tp, fn, fp, tn
-    
-    
-def precision(output, target, thres):
-    (tp, fn, fp, tn) = tp_fn_fp_tn(output, target, thres)
-    if (tp + fp) == 0: 
+
+
+def precision(output, target):
+    (tp, fn, fp, tn) = tp_fn_fp_tn(output, target)
+    if (tp + fp) == 0:
         return 0
     else:
         return float(tp) / (tp + fp)
-    
-    
-def recall(output, target, thres):
-    (tp, fn, fp, tn) = tp_fn_fp_tn(output, target, thres)
+
+
+def recall(output, target):
+    (tp, fn, fp, tn) = tp_fn_fp_tn(output, target)
     if (tp + fn) == 0:
         return 0
     else:
         return float(tp) / (tp + fn)
-    
-    
+
+
 def f_value(prec, rec):
     if (prec + rec) == 0:
         return 0
@@ -135,7 +137,7 @@ def f_value(prec, rec):
 
 def roc_auc(output, target):
     return metrics.roc_auc_score(target, output)
-    
-    
+
+
 def average_precision(output, target):
     return metrics.average_precision_score(target, output)